70fd0d7cbb92b66c98494286b9714db6454c80ec
Cristian Gavilan <cristiangavilan@gmail.com>
2020-08-04 00:42:04 -0300

    add docs

---
+++ docs/precache-manifest.de58d23c3dbcfa6d8b99e3d975a3da33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3b7dc81c397d23fbad8ca41d5f713c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "757725cb67b3dabba508",
+    "url": "/static/css/main.692a0f27.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "757725cb67b3dabba508",
+    "url": "/static/js/main.422c6403.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
